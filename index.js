1f18c87ed25a25f2d60467ffa92e30f1b82a3625
Jesse Smith <sosaucily@gmail.com>
2022-05-26 13:27:42 +0200

    add next methods for interfacing with rails

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 
-const app = require('express')();
+const express = require('express')
+const app = express();
+app.use(express.json());
 const fetch = require('node-fetch');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const { ethers } = require("ethers");
 
 const port = process.env.PORT || 8000;
+
 
 const abi = require('./ABIs/DiscreetLog.json');
 
@@ -21,13 +24,22 @@ const address = ADDRESS;
 
 const contract = new ethers.Contract(address, abi, provider);
 
+console.log(`Listening to events on ${address} \n`);
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-console.log(`Listening to events on ${address} \n`);
+//This accepts calls from the rails server to add the DLC with UUID into the smart contract
+app.post('/addNewDLC', (req, res) => {
+  console.log('pushing back to chain', req.body);
+  res.send(req.body);
+  addNewDLC(req.body.uuid, req.body.feedAddress, req.body.closingTime, req.body.emergencyRefundTime); //write into the contract.
+});
 
-async function createDLC(feedAddress, closingTime, emergencyRefundTime, caller) {
+async function addNewDLC(uuid, feedAddress, closingTime, emergencyRefundTime); //write into the blockchain contract.
+
+async function requestCreateDLC(feedAddress, closingTime, emergencyRefundTime, caller) {
   currencySymbol = await getCurrencyNameFromCLContractAddress(feedAddress)
   const body = {
     clFeedUrl: feedAddress,
@@ -38,6 +50,38 @@ async function createDLC(feedAddress, closingTime, emergencyRefundTime, caller) 
   }
   try {
     const response = await fetch(process.env.ORACLE_INTERFACE_DOMAIN + '/contracts', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify(body)
+    })
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText }
+    }
+    const data = await response.json()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data }),
+    }
+  } catch (error) {
+    // output to netlify function log
+    console.log(error)
+    return {
+      statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: error.message }),
+    }
+  }
+}
+
+async function closeDLC(uuid, price) {
+  const body = {
+    price: price,
+    uuid: uuid,
+  }
+  try {
+    const response = await fetch(process.env.ORACLE_INTERFACE_DOMAIN + '/contracts/close', {
       headers: { 'Content-Type': 'application/json' },
       method: 'POST',
       body: JSON.stringify(body)
@@ -89,16 +133,16 @@ async function getCurrencyNameFromCLContractAddress(address) {
   }
 }
 
-provider.on("block", (blockNumber) => {
-  io.emit('eth event', `New block: ${blockNumber}`);
-  console.log(`New block: ${blockNumber}`);
-})
+// provider.on("block", (blockNumber) => {
+//   io.emit('eth event', `New block: ${blockNumber}`);
+//   console.log(`New block: ${blockNumber}`);
+// })
 
 contract.on("RequestCreateDLC", (feedAddress, closingTime, emergencyRefundTime, caller) => {
   const currentTime = new Date();
   io.emit('eth event', `New DLC request @ ${currentTime} \n\t feedAddr: ${feedAddress} | closingTime: ${closingTime} | emergencyRefundTime: ${emergencyRefundTime} | caller: ${caller} \n`);
   console.log(`New DLC request @ ${currentTime} \n\t feedAddr: ${feedAddress} | closingTime: ${closingTime} | emergencyRefundTime: ${emergencyRefundTime} | caller: ${caller} \n`);
-  const response = createDLC(feedAddress, closingTime, emergencyRefundTime, caller);
+  const response = requestCreateDLC(feedAddress, closingTime, emergencyRefundTime, caller);
   console.log(response);
 });
 
@@ -107,11 +151,15 @@ contract.on("NewDLC", (uuid, feedAddress, closingTime, emergencyRefundTime) => {
   io.emit('eth event', `New DLC created @ ${currentTime} \n\t uuid: ${uuid} | feedAddr: ${feedAddress} | closingTime: ${closingTime} | emergencyRefundTime: ${emergencyRefundTime} \n`);
   console.log(`New DLC created @ ${currentTime} \n\t uuid: ${uuid} | feedAddr: ${feedAddress} | closingTime: ${closingTime} | emergencyRefundTime: ${emergencyRefundTime} \n`);
 });
+
 contract.on("CloseDLC", (uuid, price, actualClosingTime) => {
   const currentTime = new Date();
   io.emit('eth event', `Closing DLC @ ${currentTime} \n\t uuid: ${uuid} | price: ${price} | actualClosingTime: ${actualClosingTime} \n`);
   console.log(`Closing DLC @ ${currentTime} \n\t uuid: ${uuid} | price: ${price} | actualClosingTime: ${actualClosingTime} \n`);
+  const response = closeDLC(uuid, price);
+  console.log(response);
 });
+
 contract.on("EarlyCloseDLC", (uuid, price, actualClosingTime) => {
   const currentTime = new Date();
   io.emit('eth event', `Early closing DLC @ ${currentTime} \n\t uuid: ${uuid} | price: ${price} | actualClosingTime: ${actualClosingTime} \n`);
@@ -122,9 +170,8 @@ http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
 
-
-
 (async function main() {
-  const response = await createDLC("0x6135b13325bfC4B00278B4abC5e20bbce2D6580e", "2022-05-26", "2022-05-26", "0xasdfa");
+  // const response = await requestCreateDLC("0x6135b13325bfC4B00278B4abC5e20bbce2D6580e", "2022-05-26", "2022-05-26", "0xasdfab");
+  // const response = await closeDLC("12345", "60");
   console.log(response);
 })();
